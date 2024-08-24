0ef96addb0e6b954c30d61e8e9ce1066417fa526
Jayanta66 <jayhalder18@gmail.com>
2024-08-24 10:45:06 +0530

    iitb-config-server

---
+++ src/test/java/com/iitb/iitb_config_server/IitbConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitb.iitb_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IitbConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
